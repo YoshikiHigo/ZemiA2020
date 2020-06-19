6c0147b759807fd5d6782912bab09079234d5c6f
Yoshiki Higo <yoshiki.higo@gmail.com>
2020-06-19 09:50:09 +0900

    initial commit

---
+++ src/main/java/zemiA/ZemiAMain.java
@@ -0,0 +1,38 @@
+package zemiA;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+
+public class ZemiAMain {
+
+  public static void main(final String[] args) {
+    List<String> lines = null;
+    try {
+      lines = Files.readAllLines(Paths.get("src/main/java/zemiA/ZemiAMain.java"),
+          StandardCharsets.ISO_8859_1);
+    } catch (final Exception e) {
+      System.err.println(e.getMessage());
+      return;
+    }
+
+    final ASTParser parser = ASTParser.newParser(AST.JLS14);
+    parser.setSource(String.join(System.lineSeparator(), lines).toCharArray());
+
+    CompilationUnit unit = null;
+    try {
+      unit = (CompilationUnit) parser.createAST(new NullProgressMonitor());
+    } catch (final Exception e) {
+      System.err.println(e.getMessage());
+      return;
+    }
+
+    final ZemiAVisitor visitor = new ZemiAVisitor();
+    unit.accept(visitor);
+  }
+}
---
+++ src/main/java/zemiA/ZemiAVisitor.java
@@ -0,0 +1,21 @@
+package zemiA;
+
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.core.dom.Statement;
+
+public class ZemiAVisitor extends ASTVisitor {
+
+  @Override
+  public boolean visit(SimpleName node) {
+    System.out.println(node.getIdentifier());
+    return super.visit(node);
+  }
+
+  @Override
+  public boolean visit(MethodDeclaration node) {
+    //System.out.println(node.toString());
+    return super.visit(node);
+  }
+}
