ba9c14a98e55e3c81a3d9209bc1c1b4bf5e35aa3
KazukiHasegawa831 <u087279c@ecs.osaka-u.ac.jp>
2020-07-03 14:21:35 +0900

    add some metrics

--- src/main/java/zemiA/ZemiAVisitor.java
+++ src/main/java/zemiA/ZemiAVisitor.java
@@ -3,19 +3,36 @@ package zemiA;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.SimpleName;
-import org.eclipse.jdt.core.dom.Statement;
 
 public class ZemiAVisitor extends ASTVisitor {
 
-  @Override
-  public boolean visit(SimpleName node) {
-    System.out.println(node.getIdentifier());
-    return super.visit(node);
-  }
+	int NOM = 0;
+	int BOvR = 0;
 
-  @Override
-  public boolean visit(MethodDeclaration node) {
-    //System.out.println(node.toString());
-    return super.visit(node);
-  }
+	@Override
+	public boolean visit(SimpleName node) {
+		String override = "Override";
+		System.out.println(node.toString());
+		if(override.equals(node.toString())) {
+			BOvR++;
+			System.out.println("BOvR = " + BOvR);
+		}
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(MethodDeclaration node) {
+		System.out.println(node.toString());
+		return super.visit(node);
+	}
+
+	@Override
+	public void endVisit(MethodDeclaration node) {//メソッドをvisitした後に呼び出される
+		NOM++;
+		System.out.println("NOM = " + NOM);
+		// TODO 自動生成されたメソッド・スタブ
+		super.endVisit(node);
+	}
+
+
 }
