4d615b92d7524407624a7dbd3cf260eaa751a2f9
KazukiHasegawa831 <u087279c@ecs.osaka-u.ac.jp>
2020-07-10 13:44:57 +0900

    comment

--- src/main/java/zemiA/ZemiAMain.java
+++ src/main/java/zemiA/ZemiAMain.java
@@ -4,6 +4,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
@@ -11,32 +12,35 @@ import org.eclipse.jdt.core.dom.CompilationUnit;
 
 public class ZemiAMain {
 
-  public static void main(final String[] args) {
-    List<String> lines = null;
-    try {
-      lines = Files.readAllLines(Paths.get("src/main/java/zemiA/ZemiAMain.java"),
-          StandardCharsets.ISO_8859_1);
-    } catch (final Exception e) {
-      System.err.println(e.getMessage());
-      return;
-    }
+	public static void main(final String[] args) {
+		List<String> lines = null;
+		try {
+			lines = Files.readAllLines(Paths.get("src/main/java/zemiA/ZemiAMain.java"),
+					StandardCharsets.ISO_8859_1);
+		} catch (final Exception e) {
+			System.err.println(e.getMessage());
+			return;
+		}
 
-    final ASTParser parser = ASTParser.newParser(AST.JLS14);
-    parser.setSource(String.join(System.lineSeparator(), lines).toCharArray());
+		final ASTParser parser = ASTParser.newParser(AST.JLS14);
+		parser.setSource(String.join(System.lineSeparator(), lines).toCharArray());
 
-    CompilationUnit unit = null;
-    try {
-      unit = (CompilationUnit) parser.createAST(new NullProgressMonitor());
-    } catch (final Exception e) {
-      System.err.println(e.getMessage());
-      return;
-    }
+		CompilationUnit unit = null;
+		try {
+			unit = (CompilationUnit) parser.createAST(new NullProgressMonitor());
+		} catch (final Exception e) {
+			System.err.println(e.getMessage());
+			return;
+		}
 
-    if(true){
-      System.out.println("ABC");
-    }
+		if (true) {
+			System.out.println("ABC");
+		}
 
-    final ZemiAVisitor visitor = new ZemiAVisitor();
-    unit.accept(visitor);
-  }
+		final ZemiAVisitor visitor = new ZemiAVisitor();
+		unit.accept(visitor);
+
+		final NumberOfMethod NOM = new NumberOfMethod();
+		unit.accept(NOM);
+	}
 }
--- src/main/java/zemiA/ZemiAVisitor.java
+++ src/main/java/zemiA/ZemiAVisitor.java
@@ -1,29 +1,28 @@
 package zemiA;
 
 import org.eclipse.jdt.core.dom.ASTVisitor;
-import org.eclipse.jdt.core.dom.IfStatement;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.MethodInvocation;
 import org.eclipse.jdt.core.dom.SimpleName;
-import org.eclipse.jdt.core.dom.Statement;
 
 public class ZemiAVisitor extends ASTVisitor {
 
-  @Override
-  public boolean visit(SimpleName node) {
-    //System.out.println(node.getIdentifier());
-    return super.visit(node);
-  }
+	@Override
+	public boolean visit(SimpleName node) {
+		System.out.println(node.getIdentifier());
+		return super.visit(node);
+	}
 
-  @Override
-  public boolean visit(MethodDeclaration node) {
-    //System.out.println(node.toString());
-    return super.visit(node);
-  }
+	@Override
+	public boolean visit(MethodDeclaration node) {
+		//System.out.println(node.toString());
+		return super.visit(node);
+	}
 
-  @Override
-  public boolean visit(final MethodInvocation node) {
-    System.out.println(node.toString());
-    return super.visit(node);
-  }
+	@Override
+	public boolean visit(final MethodInvocation node) {
+		//System.out.println(node.toString());
+		return super.visit(node);
+	}
+
 }
