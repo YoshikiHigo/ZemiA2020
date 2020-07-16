e768f8959b16b497ee188dba8fff8df7aa43e57f
hondadada <hondy1235@gmail.com>
2020-07-16 21:45:21 +0900

    main

--- src/main/java/zemiA/ZemiAMain.java
+++ src/main/java/zemiA/ZemiAMain.java
@@ -1,46 +1,61 @@
 package zemiA;
 
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
-
+import FileLoader.FileLoader;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+class Metrics{
+	int NProtM;
+	double BUR;
+	double BOvR;
+	double AMW;
+	int WMC;
+	int NOM;
+}
+
 public class ZemiAMain {
 
 	public static void main(final String[] args) {
-		List<String> lines = null;
-		try {
-			lines = Files.readAllLines(Paths.get("src/main/java/zemiA/ZemiAMain.java"),
-					StandardCharsets.ISO_8859_1);
-		} catch (final Exception e) {
-			System.err.println(e.getMessage());
-			return;
+		if (args[1] == null){
+			System.out.println("please write folder path");
+			System.exit(0);
 		}
 
-		final ASTParser parser = ASTParser.newParser(AST.JLS14);
-		parser.setSource(String.join(System.lineSeparator(), lines).toCharArray());
+		FileLoader fileloader = FileLoader.GetInstance();
 
-		CompilationUnit unit = null;
-		try {
-			unit = (CompilationUnit) parser.createAST(new NullProgressMonitor());
-		} catch (final Exception e) {
-			System.err.println(e.getMessage());
-			return;
+		if(!fileloader.Init(args[1])){
+			System.out.println("please write folder path");
+			System.exit(0);
 		}
 
-		if (true) {
-			System.out.println("ABC");
+		List<String> classNames = fileloader.GetAllClassNames();
+
+		for (String className : classNames){
+			List<String> lines = null;
+			lines = fileloader.GetJavaFile(className);
+
+			final ASTParser parser = ASTParser.newParser(AST.JLS14);
+			parser.setSource(String.join(System.lineSeparator(), lines).toCharArray());
+
+			CompilationUnit unit = null;
+			try {
+				unit = (CompilationUnit) parser.createAST(new NullProgressMonitor());
+			} catch (final Exception e) {
+				System.err.println(e.getMessage());
+				return;
+			}
+
+			final ZemiAVisitor visitor = new ZemiAVisitor();
+			unit.accept(visitor);
+
+			//ここから各機能のクラスを実行
 		}
-
-		final ZemiAVisitor visitor = new ZemiAVisitor();
-		unit.accept(visitor);
-
-		final NumberOfMethod NOM = new NumberOfMethod();
-		unit.accept(NOM);
 	}
 }
